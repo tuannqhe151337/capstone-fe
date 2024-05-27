70f8da24853eb52a2b542bc0b90f8dcff8a7f4f0
hoannbhe160107 <hoannbhe160107@fpt.edu.vn>
2024-05-27 23:06:09 +0700

    [ui] fix z-index and animate 3 bubbles

--- src/pages/auth-root-page/index.tsx
+++ src/pages/auth-root-page/index.tsx
@@ -3,6 +3,7 @@ import { Variants, motion } from "framer-motion";
 import { ThemeChanger } from "../../features/theme-changer";
 import { LanguageChanger } from "../../features/language-changer";
 import { DarkmodeChanger } from "../../features/darkmode-changer";
+import { BubbleBackground } from "../../entities/bubble-background";
 
 enum AnimationStage {
   HIDDEN = "hidden",
@@ -27,7 +28,7 @@ const imageAnimation: Variants = {
 export const AuthRootPage: React.FC = () => {
   return (
     <div className="flex flex-row flex-wrap w-full">
-      <div className="flex flex-row flex-wrap items-center w-full">
+      <div className="flex flex-row flex-wrap items-center w-full z-20">
         <div className=" text-5xl text-primary-500 ml-16 p-6">
           <span className="text-4xl font-black">F</span>
           <span className="text-3xl font-extrabold">in</span>
@@ -35,22 +36,16 @@ export const AuthRootPage: React.FC = () => {
           <span className="text-3xl font-extrabold">lanning</span>
         </div>
 
-        <div className="ml-auto flex flex-row flex-wrap items-center pr-10">
-          <ThemeChanger></ThemeChanger>
-          <DarkmodeChanger></DarkmodeChanger>
+        <div className="ml-auto flex flex-row flex-wrap items-center pr-10 z-20">
           <div className="ml-1.5">
             <LanguageChanger></LanguageChanger>
           </div>
+          <ThemeChanger></ThemeChanger>
+          <DarkmodeChanger></DarkmodeChanger>
         </div>
       </div>
 
-      <div className="absolute min-h-screen min-w-full -z-50 inset-0">
-        <div className="relative min-h-screen overflow-hidden">
-          <div className="absolute rounded-full size-[700px] bg-primary-300 dark:bg-primary-900/50 -top-32 -right-40 opacity-40"></div>
-          <div className="absolute rounded-full size-[300px] bg-primary-200 dark:bg-primary-950 top-[400px] -right-40 opacity-40 "></div>
-          <div className="absolute rounded-full size-[300px] bg-primary-300 dark:bg-primary-950 -bottom-32 -left-32 opacity-70"></div>
-        </div>
-      </div>
+     <BubbleBackground></BubbleBackground>
 
       <div className="flex flex-row flex-wrap w-full">
         <div className="flex-1">
--- src/pages/login-page/index.tsx
+++ src/pages/login-page/index.tsx
@@ -37,73 +37,72 @@ const childrenAnimation: Variants = {
 };
 
 export const LoginPage: React.FC = () => {
-  
   const { t } = useTranslation(["login"]);
-  
-  return   <div className="flex-1">
-  <div className="flex justify-center items-center h-full">
-    <motion.div
-      className="w-[560px] "
-      initial={AnimationStage.HIDDEN}
-      animate={AnimationStage.VISIBLE}
-      variants={staggerChildrenAnimation}
-    >
-      <motion.div
-        variants={{
-          [AnimationStage.HIDDEN]: {
-            opacity: 0,
-            y: 10,
-          },
-          [AnimationStage.VISIBLE]: {
-            opacity: 1,
-            y: 0,
-          },
-        }}
-        className="mb-8 font-bold text-center text-3xl text-primary-500"
-      >
-        {t("login")}
-      </motion.div>
 
-      <motion.div variants={childrenAnimation}>
-        <TEInput
-          type="text"
-          id=""
-          label="Username"
-          className="mb-4 w-full !p-2 bg-white dark:bg-neutral-900 "
-          size="lg"
-        ></TEInput>
-      </motion.div>
-
-      <motion.div variants={childrenAnimation}>
-        <TEInput
-          type="text"
-          id=""
-          label="Password"
-          className="mb-4 w-full !p-2 bg-white dark:bg-neutral-900"
-          size="lg"
-        ></TEInput>
-      </motion.div>
-
-      <motion.div className="mb-8" variants={childrenAnimation}>
-        <a
-          href="#!"
-          className="ml-auto w-fit text-bold underline block text-primary-500 transition duration-150 ease-in-out hover:text-primary-600 focus:text-primary-600 active:text-primary-700 dark:text-primary-400 dark:hover:text-primary-500 dark:focus:text-primary-500 dark:active:text-primary-600"
+  return (
+    <div className="flex-1 z-10">
+      <div className="flex justify-center items-center h-full">
+        <motion.div
+          className="w-[560px] "
+          initial={AnimationStage.HIDDEN}
+          animate={AnimationStage.VISIBLE}
+          variants={staggerChildrenAnimation}
         >
-          {t("alreadyHaveAnAccount")}
-        </a>
-      </motion.div>
-
-      <motion.div variants={childrenAnimation}>
-        <TERipple className="w-full">
-          <button
-            type="button"
-            className="!p-3 w-full inline-block rounded bg-primary-500 px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
+          <motion.div
+            variants={{
+              [AnimationStage.HIDDEN]: {
+                opacity: 0,
+                y: 10,
+              },
+              [AnimationStage.VISIBLE]: {
+                opacity: 1,
+                y: 0,
+              },
+            }}
+            className="mb-8 font-bold text-center text-3xl text-primary-500"
           >
             {t("login")}
-          </button>
-        </TERipple>
-      </motion.div>
-    </motion.div>
-  </div>
-</div>;
+          </motion.div>
+
+          <motion.div variants={childrenAnimation}>
+            <TEInput
+              type="text"
+              label="Username"
+              className="mb-4 w-full bg-white dark:bg-neutral-900 "
+              size="lg"
+            ></TEInput>
+          </motion.div>
+
+          <motion.div variants={childrenAnimation}>
+            <TEInput
+              type="text"
+              label="Password"
+              className="mb-4 w-full bg-white dark:bg-neutral-900"
+              size="lg"
+            ></TEInput>
+          </motion.div>
+
+          <motion.div className="mb-8" variants={childrenAnimation}>
+            <a
+              href="#!"
+              className="ml-auto w-fit text-bold underline block text-primary-500 transition duration-150 ease-in-out hover:text-primary-600 focus:text-primary-600 active:text-primary-700 dark:text-primary-400 dark:hover:text-primary-500 dark:focus:text-primary-500 dark:active:text-primary-600"
+            >
+              {t("alreadyHaveAnAccount")}
+            </a>
+          </motion.div>
+
+          <motion.div variants={childrenAnimation}>
+            <TERipple className="w-full">
+              <button
+                type="button"
+                className="!p-3 w-full inline-block rounded bg-primary-500 px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
+              >
+                {t("login")}
+              </button>
+            </TERipple>
+          </motion.div>
+        </motion.div>
+      </div>
+    </div>
+  );
 };
