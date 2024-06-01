fa4dfd9742019d269b07c9415b7e34131fd5d5c0
hoannbhe160107 <hoannbhe160107@fpt.edu.vn>
2024-06-01 22:04:51 +0700

    [ui] fix button plan status

--- src/widgets/table-plan/index.tsx
+++ src/widgets/table-plan/index.tsx
@@ -1,6 +1,9 @@
 import { FaPlusCircle, FaTrash } from "react-icons/fa";
 import { IconButton } from "../../shared/icon-button";
 import { Pagination } from "../../shared/pagination";
+import { useState } from "react";
+import { motion, Variants } from "framer-motion";
+import { Tag } from "../../shared/tag";
 
 // Định nghĩa kiểu cho status
 type StatusType = "new" | "approve" | "waiting for approval" | "denied";
@@ -8,39 +11,27 @@ const renderButton = (status: StatusType) => {
   switch (status) {
     case "new":
       return (
-        <button
-          type="button"
-          className="border select-none shadow-sm rounded px-4 ml-4 mt-2 dark:shadow-black text-green-600 font-bold dark:bg-neutral-700 group-hover:text-green-700 dark:group-hover:text-green-500 dark:group-hover:bg-neutral-800"
-        >
+        <Tag background="unfilled" variant="new">
           New
-        </button>
+        </Tag>
       );
     case "approve":
       return (
-        <button
-          type="button"
-          className=" border select-none rounded px-6 py-1 ml-4 bg-green-600 dark:shadow-black dark:bg-green-800 dark:text-white/70 text-white font-bold group-hover:text-white group-hover:bg-green-700 dark:group-hover:bg-green-900 dark:group-hover:text-white/80"
-        >
-          Approve
-        </button>
+        <Tag background="filled" variant="reviewed">
+          Reviewed
+        </Tag>
       );
     case "waiting for approval":
       return (
-        <button
-          type="button"
-          className="border select-none px-4 py-1 ml-4 bg-white shadow-sm dark:shadow-black rounded font-bold text-primary-500 dark:text-primary-600 dark:bg-white/80 group-hover:text-primary-600 dark:group-hover:text-primary-600 dark:group-hover:bg-white/90"
-        >
+        <Tag background="unfilled" variant="waiting">
           Waiting for approval
-        </button>
+        </Tag>
       );
     case "denied":
       return (
-        <button
-          type="button"
-          className="border select-none ml-4 px-4 py-1 bg-white dark:bg-white/80 dark:bg-neutral dark:shadow-black rounded text-red-500 font-bold shadow-sm group-hover:text-red-600 dark:group-hover:text-red-500/90 dark:group-hover:bg-white/90"
-        >
+        <Tag background="unfilled" variant="denied">
           Denined
-        </button>
+        </Tag>
       );
     default:
       return null;
@@ -49,6 +40,7 @@ const renderButton = (status: StatusType) => {
 
 // Định nghĩa kiểu cho dữ liệu bảng
 type TablePlanDataType = {
+  id: number;
   plan: string;
   term: string;
   department: string;
@@ -58,6 +50,7 @@ type TablePlanDataType = {
 
 const tablePlanDataList: TablePlanDataType[] = [
   {
+    id: 1,
     plan: "BU name_termplan",
     term: "Financial plan December Q3 2021",
     department: "BU 1",
@@ -65,6 +58,7 @@ const tablePlanDataList: TablePlanDataType[] = [
     status: "new",
   },
   {
+    id: 2,
     plan: "BU name_termplan",
     term: "Financial plan December Q3 2021",
     department: "BU 1",
@@ -72,6 +66,7 @@ const tablePlanDataList: TablePlanDataType[] = [
     status: "approve",
   },
   {
+    id: 3,
     plan: "BU name_termplan",
     term: "Financial plan December Q3 2021",
     department: "BU 1",
@@ -79,6 +74,7 @@ const tablePlanDataList: TablePlanDataType[] = [
     status: "waiting for approval",
   },
   {
+    id: 4,
     plan: "BU name_termplan",
     term: "Financial plan December Q3 2021",
     department: "BU 1",
@@ -87,7 +83,29 @@ const tablePlanDataList: TablePlanDataType[] = [
   },
 ];
 
+enum AnimationStage {
+  HIDDEN = "hidden",
+  VISIBLE = "visible",
+}
+
+const animation: Variants = {
+  hidden: {
+    opacity: 0,
+    transition: {
+      duration: 0.25,
+    },
+  },
+  visible: {
+    opacity: 1,
+    transition: {
+      duration: 0.25,
+    },
+  },
+};
+
 export const TablePlanManagement: React.FC = () => {
+  const [hoverRowIndex, setHoverRowIndex] = useState<number>();
+
   return (
     <div>
       <table className="text-center text-sm font-light mt-6 min-w-full shadow overflow-hidden rounded">
@@ -118,7 +136,7 @@ export const TablePlanManagement: React.FC = () => {
             >
               Version
             </th>
-            <th scope="col" className="px-6 py-4">
+            <th scope="col">
               <IconButton className="px-3">
                 <FaPlusCircle className="text-2xl text-primary-500/80 hover:text-primary-500/80 mt-1" />
               </IconButton>
@@ -128,12 +146,18 @@ export const TablePlanManagement: React.FC = () => {
         <tbody>
           {tablePlanDataList.map((row, index) => (
             <tr
-              key={index}
+              key={row.id}
               className={`group border-b cursor-pointer ${
                 index % 2 === 0
-                  ? "bg-white hover:bg-primary-50/50 dark:border-neutral-500 dark:bg-neutral-800/70 dark:hover:bg-neutral-900"
-                  : "bg-primary-100 hover:bg-primary-200 dark:border-neutral-500 dark:bg-primary-950/50 dark:hover:bg-primary-950"
+                  ? "bg-white hover:bg-primary-50/50 dark:border-neutral-900 dark:bg-neutral-800/70 dark:hover:bg-neutral-800"
+                  : "bg-primary-50 hover:bg-primary-100 dark:border-neutral-900 dark:bg-primary-950/30 dark:hover:bg-primary-950/70"
               }`}
+              onMouseEnter={() => {
+                setHoverRowIndex(index);
+              }}
+              onMouseLeave={() => {
+                setHoverRowIndex(undefined);
+              }}
             >
               <td className="whitespace-nowrap px-6 py-4 font-medium">
                 <div className="flex flex-row flex-wrap">
@@ -154,9 +178,20 @@ export const TablePlanManagement: React.FC = () => {
                 {row.version}
               </td>
               <td className="whitespace-nowrap px-6 py-4">
-                <IconButton>
-                  <FaTrash className="text-red-600 text-xl opacity-0 group-hover:opacity-100 transition-opacity" />
-                </IconButton>
+                <motion.div
+                  initial={AnimationStage.HIDDEN}
+                  animate={
+                    hoverRowIndex === index
+                      ? AnimationStage.VISIBLE
+                      : AnimationStage.HIDDEN
+                  }
+                  exit={AnimationStage.HIDDEN}
+                  variants={animation}
+                >
+                  <IconButton>
+                    <FaTrash className="text-red-600 text-xl" />
+                  </IconButton>
+                </motion.div>
               </td>
             </tr>
           ))}
