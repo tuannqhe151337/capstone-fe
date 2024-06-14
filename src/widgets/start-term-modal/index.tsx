ecc0f2ca536ab1a2b98a3c82bbcbccc4d3cf8639
tuannqhe151337 <tuannqhe151337@fpt.edu.vn>
2024-06-14 17:12:14 +0700

    [ui] fix modal styles

--- src/widgets/start-term-modal/index.tsx
+++ src/widgets/start-term-modal/index.tsx
@@ -1,10 +1,9 @@
 import { IconButton } from "../../shared/icon-button";
 import { Modal } from "../../shared/modal";
 import { IoClose } from "react-icons/io5";
-
-import { FaCheckCircle } from "react-icons/fa";
 import { Button } from "../../shared/button";
 import { RiErrorWarningFill } from "react-icons/ri";
+import { FaExclamation } from "react-icons/fa6";
 
 interface Props {
   show: boolean;
@@ -14,11 +13,11 @@ interface Props {
 export const StartTermModal: React.FC<Props> = ({ show, onClose }) => {
   return (
     <Modal
-      className="w-[70vw] xl:w-[60vw] h-[72vh] flex flex-col justify-center items-center"
+      className="w-[70vw] xl:w-[50vw] h-max flex flex-col justify-center items-center"
       show={show}
       onClose={onClose}
     >
-      <div className="relative pt-5 w-full h-full flex flex-col items-center justify-center">
+      <div className="relative w-full h-full flex flex-col items-center justify-center px-10 py-8">
         <div className="absolute top-3 right-5">
           <IconButton
             className="hover:bg-neutral-100"
@@ -31,16 +30,22 @@ export const StartTermModal: React.FC<Props> = ({ show, onClose }) => {
           </IconButton>
         </div>
 
-        <div className="flex flex-col items-center gap-4">
-          <RiErrorWarningFill className="text-[160px] text-primary-500 dark:opacity-80" />
+        <div className="flex flex-col items-center">
+          <div className="flex flex-row flex-wrap items-center justify-center size-[100px] bg-red-100 dark:bg-red-900/50 rounded-full">
+            <FaExclamation className="text-[42px] text-red-500/80 dark:text-red-700/80" />
+          </div>
 
-          <div className="font-bold text-2xl text-primary-600 dark:text-primary-600 mt-4">
+          <div className="font-bold text-2xl text-neutral-400 dark:text-neutral-500 mt-5">
             Start term
           </div>
-          <div className="font-bold text-primary-600 dark:text-primary-600 mt-2">
-            You're going to start term "Finalcial plan December Q3 2021"
+          <div className="font-semibold dark:font-bold text-neutral-400 dark:text-neutral-500 mt-5">
+            You're going to start term
+            <span className="font-extrabold dark:text-neutral-400/70">
+              "Finalcial plan December Q3 2021"
+            </span>
           </div>
-          <div className="font-bold text-primary-600 dark:text-primary-600 mt-2">
+
+          <div className="mt-3 font-semibold dark:font-bold text-neutral-400 dark:text-neutral-500">
             This action <span className="font-extrabold">can not</span> be
             reversed. Are you sure?
           </div>
--- src/widgets/user-active-confirm-modal/index.tsx
+++ src/widgets/user-active-confirm-modal/index.tsx
@@ -13,11 +13,11 @@ interface Props {
 export const ActiveConfirmModal: React.FC<Props> = ({ show, onClose }) => {
   return (
     <Modal
-      className="w-[70vw] xl:w-[56vw] h-[70vh] flex flex-col justify-center items-center"
+      className="w-[70vw] xl:w-[50vw] h-max flex flex-col justify-center items-center"
       show={show}
       onClose={onClose}
     >
-      <div className="relative pt-5 w-full h-full flex flex-col items-center justify-center">
+      <div className="relative w-full h-full flex flex-col items-center justify-center px-10 py-8">
         <div className="absolute top-3 right-5">
           <IconButton
             className="hover:bg-neutral-100"
@@ -30,25 +30,37 @@ export const ActiveConfirmModal: React.FC<Props> = ({ show, onClose }) => {
           </IconButton>
         </div>
 
-        <div className="flex flex-col items-center gap-4">
-          <FaCheckCircle className="text-[140px] text-primary-500 dark:opacity-80" />
-          <div className="font-bold text-2xl text-primary-600 dark:text-primary-600 mt-4">
-            Active user
+        <div className="flex flex-col items-center">
+          <FaCheckCircle className="text-[120px] text-primary-500 dark:text-primary-800" />
+          <div className="font-bold text-2xl text-primary-500 dark:text-neutral-500 mt-5">
+            Activate user
           </div>
-          <div className="font-bold text-primary-600 dark:text-primary-600 mt-2">
-            You're going to deactivate user "AnhLN7".
+          <div className="font-semibold dark:font-bold text-primary-500 dark:text-neutral-500 mt-5">
+            You're going to activate user{" "}
+            <span className="font-extrabold dark:text-neutral-400/70">
+              "AnhLN7"
+            </span>
+            .
           </div>
-          <div className="font-bold text-primary-600 dark:text-primary-600 mt-2">
+          <div className="mt-3 font-semibold dark:font-bold text-primary-500 dark:text-neutral-500">
             Are you sure?
           </div>
         </div>
 
-        <div className="mt-8 flex flex-row gap-6">
-          <Button className="font-bold w-[300px] p-3 text-primary-500 bg-white hover:text-white hover:bg-red-500 focus:bg-red-500 focus:text-white focus:!border-red-500 border-neutral-200 active:bg-red-500 dark:text-white/80 dark:hover:text-white dark:bg-red-600 dark:border-red-600 dark:hover:bg-red-700 dark:focus:bg-red-700 dark:hover:border-red-600 dark:focus:border-red-600">
+        <div className="flex flex-row flex-wrap w-full mt-10 gap-6">
+          <Button
+            variant="tertiary"
+            className="w-[250px] p-3"
+            onClick={() => {
+              onClose && onClose();
+            }}
+          >
             No, cancel
           </Button>
 
-          <Button className="font-bold w-[300px] p-3">Yes, active user</Button>
+          <Button containerClassName="flex-1" className="font-bold p-3">
+            Yes, active user
+          </Button>
         </div>
       </div>
     </Modal>
