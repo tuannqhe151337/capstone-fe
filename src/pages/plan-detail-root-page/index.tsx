493f14c0143d439952ca110b3e786d2a114b7529
tuannqhe151337 <tuannqhe151337@fpt.edu.vn>
2024-06-17 11:24:59 +0700

    [ui] add select plan's expenses

--- src/pages/plan-detail-root-page/index.tsx
+++ src/pages/plan-detail-root-page/index.tsx
@@ -1,3 +1,4 @@
+import { Variants, motion } from "framer-motion";
 import { RiCalendarScheduleFill } from "react-icons/ri";
 import { BubbleBanner } from "../../entities/bubble-banner";
 import { Tag } from "../../shared/tag";
@@ -6,15 +7,57 @@ import { FaMoneyBillTrendUp, FaCoins } from "react-icons/fa6";
 import TabList from "../../shared/tab-list";
 import { Outlet, useNavigate } from "react-router-dom";
 
+enum AnimationStage {
+  HIDDEN = "hidden",
+  VISIBLE = "visible",
+}
+
+const staggerChildrenAnimation: Variants = {
+  [AnimationStage.HIDDEN]: {
+    transition: {
+      staggerChildren: 0.05,
+      staggerDirection: -1,
+      delayChildren: 0.15,
+      duration: 0.15,
+    },
+  },
+  [AnimationStage.VISIBLE]: {
+    transition: {
+      staggerChildren: 0.05,
+      delayChildren: 0.15,
+      duration: 0.15,
+    },
+  },
+};
+
+const childrenAnimation: Variants = {
+  [AnimationStage.HIDDEN]: {
+    opacity: 0,
+    y: 5,
+  },
+  [AnimationStage.VISIBLE]: {
+    opacity: 1,
+    y: 0,
+  },
+};
+
 export const PlanDetailRootPage: React.FC = () => {
   const navigate = useNavigate();
 
   return (
-    <div className="px-6 pb-10">
+    <motion.div
+      className="px-6 pb-10"
+      initial={AnimationStage.HIDDEN}
+      animate={AnimationStage.VISIBLE}
+      variants={staggerChildrenAnimation}
+    >
       <BubbleBanner></BubbleBanner>
 
       {/* Title */}
-      <div className="flex flex-row flex-wrap items-center mt-6 px-7">
+      <motion.div
+        className="flex flex-row flex-wrap items-center mt-6 px-7"
+        variants={childrenAnimation}
+      >
         <p className="text-2xl font-extrabold text-primary mr-5">
           Burname_templan
         </p>
@@ -27,29 +70,33 @@ export const PlanDetailRootPage: React.FC = () => {
             Waiting for review
           </Tag>
         </div>
-      </div>
+      </motion.div>
 
       <div className="flex flex-row flex-wrap justify-between gap-5 mt-10 px-5 w-full">
-        <OverviewCard
-          className="flex-1"
-          icon={<RiCalendarScheduleFill className="text-4xl" />}
-          label={"Term"}
-          value={"Financial plan December Q3 2021"}
-        />
+        <motion.div className="flex-1" variants={childrenAnimation}>
+          <OverviewCard
+            icon={<RiCalendarScheduleFill className="text-4xl" />}
+            label={"Term"}
+            value={"Financial plan December Q3 2021"}
+          />
+        </motion.div>
 
-        <OverviewCard
-          className="flex-1"
-          icon={<FaMoneyBillTrendUp className="text-4xl" />}
-          label={"Biggest expenditure"}
-          value={"180.000.000 VNĐ"}
-        />
+        <motion.div className="flex-1" variants={childrenAnimation}>
+          <OverviewCard
+            icon={<FaMoneyBillTrendUp className="text-4xl" />}
+            label={"Biggest expenditure"}
+            value={"180.000.000 VNĐ"}
+          />
+        </motion.div>
 
-        <OverviewCard
-          className="flex-1"
-          icon={<FaCoins className="text-4xl" />}
-          label={"Total plan"}
-          value={"213.425.384 VNĐ"}
-        />
+        <motion.div className="flex-1" variants={childrenAnimation}>
+          <OverviewCard
+            className="flex-1"
+            icon={<FaCoins className="text-4xl" />}
+            label={"Total plan"}
+            value={"213.425.384 VNĐ"}
+          />
+        </motion.div>
       </div>
 
       <div className="mt-7 px-5">
@@ -86,6 +133,6 @@ export const PlanDetailRootPage: React.FC = () => {
           <Outlet />
         </div>
       </div>
-    </div>
+    </motion.div>
   );
 };
