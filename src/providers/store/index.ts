22f640886b031e6d272e031b5a79e48b4867117b
hoannbhe160107 <hoannbhe160107@fpt.edu.vn>
2024-06-28 19:00:49 +0700

    [feature] link to store to redux

--- src/providers/store/index.ts
+++ src/providers/store/index.ts
@@ -2,14 +2,28 @@ import { configureStore } from "@reduxjs/toolkit";
 import darkModeReducer from "./slices/darkModeSlice";
 import { authAPI } from "./api/authApi";
 import { setupListeners } from "@reduxjs/toolkit/query";
+import { usersApi as usersAPI } from "./api/usersApi";
+import { roleAPI } from "./api/roleApi";
+import { departmentAPI } from "./api/departmentApi";
+import { positionAPI } from "./api/positionApi";
 
 export const store = configureStore({
   reducer: {
     darkMode: darkModeReducer,
     [authAPI.reducerPath]: authAPI.reducer,
+    [usersAPI.reducerPath]: usersAPI.reducer,
+    [roleAPI.reducerPath]: roleAPI.reducer,
+    [departmentAPI.reducerPath]: departmentAPI.reducer,
+    [positionAPI.reducerPath]: positionAPI.reducer,
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(authAPI.middleware),
+    getDefaultMiddleware().concat(
+      authAPI.middleware,
+      usersAPI.middleware,
+      roleAPI.middleware,
+      departmentAPI.middleware,
+      positionAPI.middleware
+    ),
 });
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
